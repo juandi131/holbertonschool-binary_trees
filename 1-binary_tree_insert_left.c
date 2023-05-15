906ce56f0e85b805171cb6f3993935c15ad5f9b2
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-15 11:51:18 -0700

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,21 +6,21 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nodo;
+	binary_tree_t *new_node;
 
-	nodo = binary_tree_node(parent, value);
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
-		nodo->left = parent->left;
-		parent->left = nodo->left;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
-	else if (parent->left == NULL)
-	{
-		parent->left = nodo->parent;
-	}
-	return (nodo);
+
+	parent->left = new_node;
+	return (new_node);
 }
