939c37cb761f2c93e6c48e13d10d176076968c78
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-14 19:23:21 -0700

    second

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,7 @@
+#ifndef _BINARIO_
+#define _BINARIO_
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +19,5 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif
