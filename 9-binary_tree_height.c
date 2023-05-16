347cec772e08c16eda72982c36edbf4e93630203
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-16 12:16:10 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
         return 0;
     }
 
-    binary_tree_height(tree->left);
-    binary_tree_height(tree->right);
+    altura_izquierda = binary_tree_height(tree->left);
+    altura_derecha = binary_tree_height(tree->right);
 
     return (altura_izquierda > altura_derecha) ? altura_izquierda + 1 : altura_derecha + 1;
 }
