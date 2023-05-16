4d0a1201a80112ffff692419b3fe8f902bf54ea7
juandi131 <juandigarcia3@gmail.com>
2023-05-16 06:29:13 -0700

    exercice

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder-
+ * @tree: this is pointer 
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL || func == NULL)
+	{
+		return ;
+   	}
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}
