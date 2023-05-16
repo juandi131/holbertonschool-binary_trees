6b3a70bca99393b8d6ad392d2d186198ada32b4c
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-16 12:09:27 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,8 +6,10 @@ typedef struct binary_tree_s {
     struct binary_tree_s *right;
 } binary_tree_t;
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    if (tree == NULL) {
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    if (tree == NULL) 
+    {
         return 0;
     }
 
