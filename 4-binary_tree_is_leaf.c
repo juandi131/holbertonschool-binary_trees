c26841d9ae14d673e5b2d812a214bf0ccd9611a1
DarianGrabino <dariangra@gmail.com>
2023-05-16 17:39:55 -0700

    modify logic

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,13 +6,7 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-	{
-		 return (0);
-	}
-	if (node->right == NULL && node->left == NULL)
-	{
-		return (1);
-	}
+	if (!node || node->right || node->left)
+		return (0);
 	return (0);
 }
