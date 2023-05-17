621b4f3fabe6c1b5edf7eaa782f0f1e922b7732a
DarianGrabino <dariangra@gmail.com>
2023-05-16 20:28:27 -0700

    change variable initialization and return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = 0;
+	right_height = 0;
+
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
+
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
