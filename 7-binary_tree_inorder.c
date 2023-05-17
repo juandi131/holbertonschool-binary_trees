efedfc559dae7e33491e71985e1ec1ae686e76fb
DarianGrabino <dariangra@gmail.com>
2023-05-16 19:11:25 -0700

    fix betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ * @func: Pointer to the function to be applied to each element of the tree.
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
