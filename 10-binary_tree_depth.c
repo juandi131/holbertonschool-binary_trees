dce625fc0572204c65ff595be1bd3673d5fecf25
DarianGrabino <dariangra@gmail.com>
2023-05-16 19:20:50 -0700

    fix betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,16 +1,19 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_depth - Calculates the depth of a node in a binary tree
+ * @tree: Pointer to the node for which depth is calculated
+*/
+size_t binary_tree_depth(const binary_tree_t *tree) 
+{
+	size_t depth = 0;
 
-size_t binary_tree_depth(const binary_tree_t *tree) {
-    size_t depth = 0;
-
-    if (tree == NULL) {
-        return 0;
-    }
-
-    while (tree->parent != NULL) {
-        depth++;
-        tree = tree->parent;
-    }
-
-    return depth;
+	if (tree == NULL) {
+		return 0;
+	}
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
