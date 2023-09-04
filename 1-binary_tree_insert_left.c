51b317d47a798d07e2e194b6ad2715ce6b237ce8
root <plaromina1997@gmail.com>
2023-09-04 08:43:41 -0700

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,31 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as the left child of another node
- * @parent: Pointer to the node to insert the left child in
- * @value: Value to store in the new node
- *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ *binary_tree_insert_left - inserts a node as the left-child of another node
+ *@parent: is a pointer to the node to insert the left-child in
+ *@value:  is the value to store in the new node
+ *Return: algo
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *nodo;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
-	node = binary_tree_node(parent, value);
-	if (!node)
+	nodo = malloc(sizeof(binary_tree_t));
+
+	if (!nodo)
 		return (NULL);
 
-	if (parent->left)
-	{
-		node->left = parent->left;
-		parent->left->parent = node;
-	}
+	nodo->left = parent->left;
+	parent->left = nodo;
+	nodo->parent = parent;
+	nodo->n = value;
+	nodo->right = NULL;
 
-	parent->left = node;
-	return (node);
+	if (nodo->left)
+		nodo->left->parent = nodo;
+	return (nodo);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,30 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -20,6 +45,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
 
 #endif
