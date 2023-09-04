8ff4456da194ada34580ae496c6a12c654b26009
root <plaromina1997@gmail.com>
2023-09-04 10:48:08 -0700

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ *binary_tree_delete - borra un arbol binario
+ *@tree: puntero al root node del arbol a borrar
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+
+    binary_tree_delete(tree->left);
+
+    binary_tree_delete(tree->right);
+
+    free(tree);
+}
