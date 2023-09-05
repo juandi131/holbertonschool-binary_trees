99f49ecddb599d7b55eb34dfd33ea46dd0bc1cdd
root <plaromina1997@gmail.com>
2023-09-05 05:58:28 -0700

    17-binary_tree_sibling.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,7 +43,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 
 	if (left_height == right_height)
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 
 	return (0);
 }
