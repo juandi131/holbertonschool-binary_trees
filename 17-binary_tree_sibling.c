e760aad825243f36b105a93d6c146255e0b87ce0
root <plaromina1997@gmail.com>
2023-09-05 06:02:56 -0700

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,23 +1,14 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_is_full - Checks if a binary tree is full
- *
- * @tree: Pointer to the root node of the tree to check
- *
- * Return: 1 if the tree is full, 0 otherwise
- */
-int binary_tree_is_full(const binary_tree_t *tree)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (tree == NULL)
-		return (0);
+	if (node == NULL || node->parent == NULL)
+	return (NULL);
 
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+	return (node->parent->left);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-
-	return (0);
+	return (NULL);
 }
